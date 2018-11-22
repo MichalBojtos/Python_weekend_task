b026bb75235719c760408f44008e1e58986cc8f2
MichalBojtos <29404856+MichalBojtos@users.noreply.github.com>
2018-11-22 17:26:04 +0100

    version 1.1
    missing option nothing and condition more then one argument option

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import bin.input_api_airports
 import bin.filters
+import argparse
+
 
 if __name__ == "__main__":
 
@@ -7,17 +9,35 @@ if __name__ == "__main__":
     jsonData = get_uk_airport.get_api()
     filters = bin.filters
 
-    iata = bin.filters.iata(jsonData)
-    print(iata)
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('--iata', action='store_true', help= 'IATA codes')
+    parser.add_argument('--cities', action='store_true', help='cities with airports')
+    parser.add_argument('--names', action='store_true', help = 'name of the airport')
+    parser.add_argument('--coords', action='store_true', help='coordinates of each airport')
+    parser.add_argument('--full', action='store_true', help='print every detail from each airport')
 
-    cities = filters.cities(jsonData)
-    print(cities)
+    args = parser.parse_args()
 
-    airportName = filters.airport_name(jsonData)
-    print(airportName)
-
-    coordinates = filters.coords(jsonData)
-    print(coordinates)
-
+    if args.iata:
+        iata = filters.iata(jsonData)
+        print(iata)
+    elif args.cities:
+        cities = filters.cities(jsonData)
+        print(cities)
+    elif args.names:
+        airportName = filters.airport_name(jsonData)
+        print(airportName)
+    elif args.coords:
+        coordinates = filters.coords(jsonData)
+        print(coordinates)
+    elif args.full:
+        iata = filters.iata(jsonData)
+        print(iata)
+        cities = filters.cities(jsonData)
+        print(cities)
+        airportName = filters.airport_name(jsonData)
+        print(airportName)
+        coordinates = filters.coords(jsonData)
+        print(coordinates)
 
 
