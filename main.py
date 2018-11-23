908426084854591ad8868a7ed10ea96fbf0838cd
MichalBojtos <29404856+MichalBojtos@users.noreply.github.com>
2018-11-23 12:52:53 +0100

    version 1.4 with nothing option

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 import pandas
 
 
-def output_iota(dt, json):
+def output_iata(dt, json):
     iata = filters.iata(json)
     column_values = pandas.Series(iata)
     dt.insert(loc=0, column='IATA', value=column_values)
@@ -17,8 +17,8 @@ def output_cities(dt, json):
 
 
 def output_names(dt, json):
-    airportName = filters.airport_name(json)
-    column_values = pandas.Series(airportName)
+    airport_name = filters.airport_name(json)
+    column_values = pandas.Series(airport_name)
     dt.insert(loc=0, column='Airport_name', value=column_values)
 
 
@@ -44,11 +44,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    '''if args:
-        output_iota(df, jsonData)
-        output_names(df, jsonData)'''
     if args.iata:
-        output_iota(df, jsonData)
+        output_iata(df, jsonData)
     if args.cities:
         output_cities(df, jsonData)
     if args.names:
@@ -56,9 +53,14 @@ if __name__ == "__main__":
     if args.coords:
         output_coords(df, jsonData)
     if args.full:
-        output_iota(df, jsonData)
+        output_iata(df, jsonData)
         output_cities(df, jsonData)
         output_names(df, jsonData)
         output_coords(df, jsonData)
 
-    print(df)
+    if df.empty:
+        output_iata(df, jsonData)
+        output_names(df, jsonData)
+        print(df)
+    else:
+        print(df)
