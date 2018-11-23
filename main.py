c224855791209d0e0d3172ae9b23afe25d5a521d
MichalBojtos <29404856+MichalBojtos@users.noreply.github.com>
2018-11-23 12:00:30 +0100

    version 1.3
    missing option nothing

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import bin.input_api_airports
 import bin.filters
 import argparse
+import pandas
+
+
+def output_iota(dt, json):
+    iata = filters.iata(json)
+    column_values = pandas.Series(iata)
+    dt.insert(loc=0, column='IATA', value=column_values)
+
+
+def output_cities(dt, json):
+    cities = filters.cities(json)
+    column_values = pandas.Series(cities)
+    dt.insert(loc=0, column='Cities', value=column_values)
+
+
+def output_names(dt, json):
+    airportName = filters.airport_name(json)
+    column_values = pandas.Series(airportName)
+    dt.insert(loc=0, column='Airport_name', value=column_values)
+
+
+def output_coords(dt, json):
+    coordinates = filters.coords(json)
+    column_values = pandas.Series(coordinates)
+    dt.insert(loc=0, column='Coordinates', value=column_values)
 
 
 if __name__ == "__main__":
@@ -8,36 +33,32 @@ if __name__ == "__main__":
     get_uk_airport = bin.input_api_airports.InputApi()
     jsonData = get_uk_airport.get_api()
     filters = bin.filters
+    df = pandas.DataFrame({})
 
     parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('--iata', action='store_true', help= 'IATA codes')
+    parser.add_argument('--iata', action='store_true', help='IATA codes')
     parser.add_argument('--cities', action='store_true', help='cities with airports')
-    parser.add_argument('--names', action='store_true', help = 'name of the airport')
+    parser.add_argument('--names', action='store_true', help='name of the airport')
     parser.add_argument('--coords', action='store_true', help='coordinates of each airport')
     parser.add_argument('--full', action='store_true', help='print every detail from each airport')
 
     args = parser.parse_args()
 
+    '''if args:
+        output_iota(df, jsonData)
+        output_names(df, jsonData)'''
     if args.iata:
-        iata = filters.iata(jsonData)
-        print(iata)
+        output_iota(df, jsonData)
     if args.cities:
-        cities = filters.cities(jsonData)
-        print(cities)
+        output_cities(df, jsonData)
     if args.names:
-        airportName = filters.airport_name(jsonData)
-        print(airportName)
+        output_names(df, jsonData)
     if args.coords:
-        coordinates = filters.coords(jsonData)
-        print(coordinates)
+        output_coords(df, jsonData)
     if args.full:
-        iata = filters.iata(jsonData)
-        print(iata)
-        cities = filters.cities(jsonData)
-        print(cities)
-        airportName = filters.airport_name(jsonData)
-        print(airportName)
-        coordinates = filters.coords(jsonData)
-        print(coordinates)
+        output_iota(df, jsonData)
+        output_cities(df, jsonData)
+        output_names(df, jsonData)
+        output_coords(df, jsonData)
 
-
+    print(df)
