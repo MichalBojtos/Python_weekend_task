bc35061af2fdb4d878d7eb12a0e95b5817a87706
MichalBojtos <29404856+MichalBojtos@users.noreply.github.com>
2018-11-24 10:20:48 +0100

    version 1.5
    changes in structure of code - new file json_to_dataframe.py

---
+++ bin/json_to_dataframe.py
@@ -0,0 +1,26 @@
+import bin.filters as filters
+import pandas
+
+
+def output_iata(dt, json):
+    iata = filters.iata(json)
+    column_values = pandas.Series(iata)
+    dt.insert(loc=0, column='IATA', value=column_values)
+
+
+def output_cities(dt, json):
+    cities = filters.cities(json)
+    column_values = pandas.Series(cities)
+    dt.insert(loc=0, column='Cities', value=column_values)
+
+
+def output_names(dt, json):
+    airport_name = filters.airport_name(json)
+    column_values = pandas.Series(airport_name)
+    dt.insert(loc=0, column='Airport_name', value=column_values)
+
+
+def output_coords(dt, json):
+    coordinates = filters.coords(json)
+    column_values = pandas.Series(coordinates)
+    dt.insert(loc=0, column='Coordinates', value=column_values)
--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-import bin.input_api_airports
-import bin.filters
+import bin.input_api_airports as api_airports
+import bin.json_to_dataframe as json_to_df
 import argparse
 import pandas
 
 
-def output_iata(dt, json):
-    iata = filters.iata(json)
-    column_values = pandas.Series(iata)
-    dt.insert(loc=0, column='IATA', value=column_values)
-
-
-def output_cities(dt, json):
-    cities = filters.cities(json)
-    column_values = pandas.Series(cities)
-    dt.insert(loc=0, column='Cities', value=column_values)
-
-
-def output_names(dt, json):
-    airport_name = filters.airport_name(json)
-    column_values = pandas.Series(airport_name)
-    dt.insert(loc=0, column='Airport_name', value=column_values)
-
-
-def output_coords(dt, json):
-    coordinates = filters.coords(json)
-    column_values = pandas.Series(coordinates)
-    dt.insert(loc=0, column='Coordinates', value=column_values)
-
-
 if __name__ == "__main__":
 
-    get_uk_airport = bin.input_api_airports.InputApi()
+    get_uk_airport = api_airports.InputApi()
     jsonData = get_uk_airport.get_api()
-    filters = bin.filters
     df = pandas.DataFrame({})
 
     parser = argparse.ArgumentParser(description='Description of your program')
@@ -45,22 +20,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.iata:
-        output_iata(df, jsonData)
+        json_to_df.output_iata(df, jsonData)
     if args.cities:
-        output_cities(df, jsonData)
+        json_to_df.output_cities(df, jsonData)
     if args.names:
-        output_names(df, jsonData)
+        json_to_df.output_names(df, jsonData)
     if args.coords:
-        output_coords(df, jsonData)
+        json_to_df.output_coords(df, jsonData)
     if args.full:
-        output_iata(df, jsonData)
-        output_cities(df, jsonData)
-        output_names(df, jsonData)
-        output_coords(df, jsonData)
+        json_to_df.output_iata(df, jsonData)
+        json_to_df.output_cities(df, jsonData)
+        json_to_df.output_names(df, jsonData)
+        json_to_df.output_coords(df, jsonData)
 
     if df.empty:
-        output_iata(df, jsonData)
-        output_names(df, jsonData)
+        json_to_df.output_iata(df, jsonData)
+        json_to_df.output_names(df, jsonData)
         print(df)
     else:
         print(df)
